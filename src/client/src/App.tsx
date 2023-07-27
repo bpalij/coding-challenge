0a6699e95ec5ea81645e290d748cb767629fef4c
Bohdan Palii <bpalij@gmail.com>
2023-07-27 11:42:20 +0300

    shows top per category by followers

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -14,16 +14,75 @@ interface dataItem {
   'Engagement avg\r\n': string,
 }
 
+interface groupedDataItem {
+  number: number;
+  item: dataItem;
+}
+
+interface groupedData {
+  [key: string]: groupedDataItem;
+}
+
+const convertStringToNumber = (str: string) => {
+  if (str[str.length - 1] === 'M') {
+    return parseFloat(str) * 1000 * 1000;
+  }
+  if (str[str.length - 1] === 'K') {
+    return parseFloat(str) * 1000;
+  }
+  return parseInt(str);
+}
+
 function App() {
 
   const [data, setData] = useState<Array<dataItem>>([]);
+  const [topPerCategoryByFollowers, setTopPerCategoryByFollowers] = useState<groupedData>({});
+
 
   useEffect(() => {
     const url = process.env.REACT_APP_DATA_URL || '';
-    console.log(url);
 
     axios.get(url).then((res: any) => {
-      setData(res.data);
+      const data = res.data as Array<dataItem>;
+      setData(data);
+      const topPerCategoryByFollowersLocal: groupedData = {};
+
+      data.forEach((item) => {
+        const {category_1, category_2, Followers} = item;
+        const followersNumber = convertStringToNumber(Followers);
+        if (!topPerCategoryByFollowersLocal[category_1]) {
+          topPerCategoryByFollowersLocal[category_1] = {
+            number: followersNumber,
+            item,
+          }
+        } else {
+          if (topPerCategoryByFollowersLocal[category_1].number < followersNumber) {
+            topPerCategoryByFollowersLocal[category_1] = {
+              number: followersNumber,
+              item,
+            }
+          }
+        };
+        if (category_2) {
+          if (!topPerCategoryByFollowersLocal[category_2]) {
+            topPerCategoryByFollowersLocal[category_2] = {
+              number: followersNumber,
+              item,
+            }
+          } else {
+            if (topPerCategoryByFollowersLocal[category_2].number < followersNumber) {
+              topPerCategoryByFollowersLocal[category_2] = {
+                number: followersNumber,
+                item,
+              }
+            }
+          };
+        }
+
+        setTopPerCategoryByFollowers(topPerCategoryByFollowersLocal);
+
+
+      })
     });
   }, []);
 
@@ -43,7 +102,7 @@ function App() {
         </tr>
         {
           data.map(item => (
-            <tr>
+            <tr key={JSON.stringify(item)}>
               <td>{item['Influencer insta name']}</td>
               <td>{item['instagram name']}</td>
               <td>{item.category_1}</td>
@@ -54,6 +113,38 @@ function App() {
               <td>{item['Engagement avg\r\n']}</td>
             </tr>
           )
+        )}
+      </table>
+      <h1>Top per category by followers</h1>
+      <table>
+        <tr>
+          <th>Leader per category</th>
+          <th>Influencer insta name</th>
+          <th>Instagram name</th>
+          <th>Category 1</th>
+          <th>Category 2</th>
+          <th>Followers</th>
+          <th>Audience country(mostly)</th>
+          <th>Authentic engagement</th>
+          <th>Engagement avg</th>
+        </tr>
+        {
+          Object.keys(topPerCategoryByFollowers).map(key => {
+            const item = topPerCategoryByFollowers[key].item;
+            return (
+              <tr key={JSON.stringify(item) + key}>
+                <td>{key}</td>
+                <td>{item['Influencer insta name']}</td>
+                <td>{item['instagram name']}</td>
+                <td>{item.category_1}</td>
+                <td>{item.category_2}</td>
+                <td>{item.Followers}</td>
+                <td>{item['Audience country(mostly)']}</td>
+                <td>{item['Authentic engagement\r\n']}</td>
+                <td>{item['Engagement avg\r\n']}</td>
+              </tr>
+            )
+          }
         )}
       </table>
     </div>
